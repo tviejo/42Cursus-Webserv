5a3298bfa728393e6fff0f072bf24d787de24c7d
Jean Teissier <jteissie@made-f0Br7s7.clusters.42paris.fr>
2024-10-07 13:57:43 +0200

    Actually adds the Array class to commit stage

--- include/Array.hpp
+++ include/Array.hpp
@@ -1,21 +1,30 @@
 #pragma once
-#include <algorithm>
 
 template <typename T, uint32_t N>
 class Array
 {
 	private:
-		const uint32_t	_max_size;
 		uint32_t		_size;
+		uint32_t	_max_size;
 		T				_array[N];
 	public:
 		Array() : _size(0), _max_size(N) {};
-		~Array();
+		~Array() = default;
 		Array(const Array& copy) : _size(copy._size), _max_size(copy._max_size) {
 			*this = copy;
 		};
+		bool operator!=(const Array &other) const {
+			if (_size != other._size || _max_size != other._max_size)
+				return true;
+			for (uint32_t i = 0; i < _size; i++)
+				if (_array[i] != other._array[i])
+				   	return true;
+			return false;
+		};
 		Array& operator=(const Array& copy) {
 			if (*this != copy){
+				_max_size = copy._max_size;
+				_size = copy._size;
 				for (uint32_t i = 0; i < _max_size; i++) {
 					_array[i] = copy._array[i];
 				}
@@ -36,13 +45,19 @@ class Array
 		bool			empty() const {
 			return _size == 0;
 		};
-		T&				front() const {
+		T&				front() {
 			return _array[0];
 		};
-		T&				back() const {
+		T&				back() {
 			return _array[_size - 1];
 		};
-		T&				at(uint32_t index) {
+		const T&				front() const {
+			return _array[0];
+		};
+		const T&				back() const {
+			return _array[_size - 1];
+		};
+		const T&				at(uint32_t index) {
 			if (index >= _max_size)
 				throw std::out_of_range("Index value out of array bounds");
 			return _array[index];
