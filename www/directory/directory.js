f95e3bb5dc097aafb62bb5bda5ef19c6d6bed546
mig29gm@gmail.com <mig29gm@gmail.com>
2024-10-25 15:55:46 +0200

    directory

--- www/directory/directory.js
+++ www/directory/directory.js
@@ -1,18 +1,25 @@
 //var images = ["1", "2", "3"];
-var prefix = "Reflections_"
-var prefixMini = "thumb/Reflections_tb"
-var extFile = ".jpg";
-var extMini = ".jpg";
+//var prefix = "Reflections_"
+//var prefixMini = "thumb/Reflections_tb"
+//var extFile = ".jpg";
+//var extMini = ".jpg";
 
-nbimages = 10;
+//nbimages = 10;
 
 //for (i in images)
-for (i=1; i<=nbimages ;i++)
+
+var entries = ["1", "2", "3"];
+
+generateDirectoryListing()
 {
-	//image = prefix+images[i];
-	num = i;
-	if (num < 10) num = "0" + num;
-	image = prefix + num;
-	miniPath = prefixMini + num + extMini;
-	document.writeln('<a href="'+image+extFile+'"><img src="'+miniPath+'" alt="'+image+'" title="'+image+'" /></a>');
+	var nbEntries = entries.length;
+	for (i=1; i<=nbEntries ;i++)
+	{
+		//image = prefix+images[i];
+		num = i;
+		if (num < 10) num = "0" + num;
+		image = prefix + num;
+		miniPath = prefixMini + num + extMini;
+		document.writeln('<a href="'+image+extFile+'"><img src="'+miniPath+'" alt="'+image+'" title="'+image+'" /></a>');
+	}
 }
