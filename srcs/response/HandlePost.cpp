2d700ae2c545966caacb6fa483c42c1d1a069fe9
Jean Teissier <jteissie@made-f0Br5s4.clusters.42paris.fr>
2024-10-26 18:36:26 +0200

    sanitizeFileName() and parseMultiPart() functions
    Add: sanitizeFilename to search and replace dangerous characters in
    *filenames* and only filenames please, do not use it for anything else
    as it replaces '/'.
    
    Add: parseMultipart() to create vector parts

--- srcs/response/HandlePost.cpp
+++ srcs/response/HandlePost.cpp
@@ -1,20 +1,20 @@
 #include "webserv.hpp"
 #include "Response.hpp"
 
-static std::string	extractFilename(const std::string& body, const std::string& filenameKey)
-{
-	size_t filenameStart = body.find(filenameKey);
-
-	if (filenameStart == std::string::npos)
-		throw std::runtime_error("No filename for uploaded file");
-	filenameStart += filenameKey.length();
-
-	size_t	filenameEnd = body.find("\"", filenameStart);
-	if (filenameEnd == std::string::npos)
-		throw std::runtime_error("No filename for uploaded file");
-
-	return body.substr(filenameStart, filenameEnd - filenameStart);
-}
+//static std::string	extractFilename(const std::string& body, const std::string& filenameKey)
+//{
+//	size_t filenameStart = body.find(filenameKey);
+//
+//	if (filenameStart == std::string::npos)
+//		throw std::runtime_error("No filename for uploaded file");
+//	filenameStart += filenameKey.length();
+//
+//	size_t	filenameEnd = body.find("\"", filenameStart);
+//	if (filenameEnd == std::string::npos)
+//		throw std::runtime_error("No filename for uploaded file");
+//
+//	return body.substr(filenameStart, filenameEnd - filenameStart);
+//}
 
 static	std::string getDate(const std::string& body)
 {
@@ -30,12 +30,22 @@ static	std::string getDate(const std::string& body)
 	return body.substr(dateStart, dateEnd - dateStart);
 }
 
+std::string sanitizeFileName(const std::string& fileName)
+{
+	std::string safe = fileName;
+	std::string banned = "\\/:*?\"<>|";
+	for (size_t i = 0; i < banned.size(); i++)
+		std::replace(safe.begin(), safe.end(), banned[i], '_');
+	return safe;
+}
+
 OutgoingData*	Response::handleTextPost(const HTTPRequest& req, const t_route& route)
 {
 	try
 	{
 		const std::string&	content = req.getBody();
 		std::string			fileName = getDate(req.getBody()) + ".txt";
+		std::string			safeFileName = sanitizeFileName(fileName);
 		const std::string	path = "www/upload/" + fileName;
 		std::string			line;
 		//set max length to protect against ddos
@@ -48,7 +58,7 @@ OutgoingData*	Response::handleTextPost(const HTTPRequest& req, const t_route& ro
 		outFile.write(content.c_str(), content.length());
 		outFile.close();
 		if (!outFile)
-			throw std::runtime_error("Could not wrote to file: " + fileName);
+			throw std::runtime_error("Could not wrote to file: " + safeFileName);
 		return makeResponse(201, "OK", "text/plain", "Text file processed succesfully: " + path);
 	}
 	catch (std::exception& e)
@@ -65,6 +75,63 @@ struct formPart
 	std::string	content;
 };
 
+std::vector<formPart>	parseMultipartForm(const std::string& body, const std::string& boundary)
+{
+	std::vector<formPart>	partsVec;
+	size_t					pos = 0;
+		
+	while (true)
+	{
+		size_t	boundaryPos = body.find(boundary, pos);
+		if (boundaryPos == std::string::npos)
+			break;
+		pos = boundaryPos + boundary.length() + 2;
+		if (body.substr(boundaryPos + boundary.length(), 4) == "--\r\n")
+			break ;
+
+		formPart	part;
+		while (pos < body.length())
+		{
+			size_t	lineEnd = body.find("\r\n", pos);
+			if (lineEnd == pos)
+			{
+				pos += 2;
+				break;
+			}
+			std::string	header = body.substr(pos, lineEnd - pos);
+			pos = lineEnd + 2;
+			if (header.find("Content-Disposition: ") == 0)
+			{
+				size_t	namePos = header.find("name=\"");
+				if (namePos != std::string::npos)
+				{
+					namePos += 6;
+					size_t	nameEnd = header.find("\"", namePos);
+					part.name = header.substr(namePos, nameEnd - namePos);
+				}
+				size_t filenamePos = header.find("filename=\"");
+				if (filenamePos != std::string::npos)
+				{
+					filenamePos += 10;
+					size_t	filenameEnd = header.find("\"", filenamePos);
+					part.fileName = header.substr(filenamePos, filenameEnd - filenamePos);
+				}
+			}
+			else if (header.find("Content-Type: ") == 0)
+				part.contentType = header.substr(14);
+		}
+		size_t	contentEnd = body.find("\r\n" + boundary, pos);
+		if (contentEnd == std::string::npos)
+			break;
+		part.content = body.substr(pos, contentEnd - pos);
+		pos = contentEnd + 2;
+		partsVec.push_back(part);
+	}
+	return partsVec;
+}
+
+
+
 OutgoingData*	Response::handleFileUpload(const HTTPRequest& req, const t_route& route)
 {
 	try 
@@ -80,7 +147,7 @@ OutgoingData*	Response::handleFileUpload(const HTTPRequest& req, const t_route& 
 					   "No boundary found in multipart/form-data");
 
 		std::string				boundary = "--" + headerIt->second.substr(boundaryPos + 9);	
-		std::vector<formPart>	parts = parseMultipartform(req.getBody(), boundary);
+		std::vector<formPart>	parts = parseMultipartForm(req.getBody(), boundary);
 
 		//create upload directory here if needed
 		std::string					uploadDir = "www/upload";
@@ -93,9 +160,11 @@ OutgoingData*	Response::handleFileUpload(const HTTPRequest& req, const t_route& 
 			if (it->content.size() > 10 * 1024 * 1024) // 10 MB
 				return makeResponse(413, "Payload Too Large", "text/plain",
 						"File size exceeds maximum allowed");
-			//std::string safeFileName = sanitizeFileName(it->fileName);
-			std::string fullPath = uploadDir + "/" + it->fileName;// + safeFileName;
-			//handle duplicate  file names here
+			std::string safeFileName = sanitizeFileName(it->fileName);
+			std::string fullPath = uploadDir + "/" + safeFileName;
+			
+			//handle duplicate  file names here with access()
+
 			std::ofstream outFile(fullPath.c_str(), std::ios::binary);
 			if (!outFile)
 				throw std::runtime_error("Failed to create outFile: " + fullPath);
@@ -103,7 +172,7 @@ OutgoingData*	Response::handleFileUpload(const HTTPRequest& req, const t_route& 
 			outFile.close();
 			if (!outFile)
 				throw std::runtime_error("Failed to write to outFile: " + fullPath);
-			uploadedFiles.push_back(it->fileName);
+			uploadedFiles.push_back(safeFileName);
 		}
 		if (uploadedFiles.empty())
 			return makeResponse(400, "Bad Request", "text/plain",
