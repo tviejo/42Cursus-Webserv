fbb9ea737e5bc1531beb249923c9bcedfa8ae550
Jean Teissier <jteissie@made-f0Br7s1.clusters.42paris.fr>
2024-11-04 15:48:00 +0100

    Changed handlePost() response from 404 to 204 for no content
    Change: Changed code and message of response in case of empty
    content-type in request from 404 "Not Found" to 204 "No Content"

--- srcs/response/Response.cpp
+++ srcs/response/Response.cpp
@@ -225,7 +225,7 @@ OutgoingData * Response::handlePost(HTTPRequest & req, int clientSocket)
 		return cgi.handleCgi();
 	}
 	else if (contentType.empty())
-		return makeErrorResponse(404, "Not Found", server, clientSocket);
+		return makeErrorResponse(204, "No Content", server, clientSocket);
 	else if (contentType.find("text/plain") != std::string::npos)
 		return handleTextPost(req, maxBodySize);
 	else if (contentType.find("multipart/form-data") != std::string::npos)
