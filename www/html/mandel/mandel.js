7fa5e807a0290adb11eb7b665bcea0d03bd74fa8
mig29gm@gmail.com <mig29gm@gmail.com>
2024-11-06 19:53:00 +0100

    Removed unnecessary JS files

--- www/html/mandel/mandel.js
+++ www/html/mandel/mandel.js
@@ -139,13 +139,6 @@ function main() {
     // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    // Clear the canvas
-    //gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    //gl.enable(gl.DEPTH_TEST);   
-    //gl.enable(gl.CULL_FACE);
-    //gl.cullFace(gl.BACK);
     gl.enable(gl.BLEND);
     //gl.blendFunc(gl.SRC_COLOR, gl.DST_COLOR);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
@@ -185,9 +178,6 @@ function main() {
     gl.uniform1i(colorShift_uloc, colorShift);
     gl.uniform1i(iterMax_uloc, iterMax);
     
-    // draw fractal
-    //gl.drawArrays(primitiveType, offset, count);
-    
     // draw kishi
     gl.useProgram(progKishi);
     //gl.bindVertexArray(vao);
@@ -212,52 +202,6 @@ function main() {
     gl.useProgram(progFract);
     if (bDrawFractal && duréeSec<55.0)
       gl.drawArrays(gl.TRIANGLES, 0, 2*3);
-    
-    /*
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-    // Every frame increase the rotation a little.
-    rotation[1] += rotationSpeed * deltaTime;
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    // Clear the canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // turn on depth testing
-    gl.enable(gl.DEPTH_TEST);
-
-    // tell webgl to cull faces
-    gl.enable(gl.CULL_FACE);
-
-    // Tell it to use our program (pair of shaders)
-    gl.useProgram(program);
-
-    // Bind the attribute/buffer set we want.
-    gl.bindVertexArray(vao);
-
-    // Compute the matrix
-    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    var zNear = 1;
-    var zFar = 2000;
-    var matrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
-    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
-    matrix = m4.xRotate(matrix, rotation[0]);
-    matrix = m4.yRotate(matrix, rotation[1]);
-    matrix = m4.zRotate(matrix, rotation[2]);
-    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
-
-    // Set the matrix.
-    gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-    // Draw the geometry.
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 16 * 6;
-    gl.drawArrays(primitiveType, offset, count);
-  */
   }
   
   var prevTime = 0;
